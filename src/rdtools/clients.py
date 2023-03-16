8420d084f19b76b37f05241e501faabc5d001843
davidruvolo51 <davidruvolo51@gmail.com>
2023-03-16 15:19:35 +0100

    init clients file

--- src/rdtools/clients.py
+++ src/rdtools/clients.py
@@ -1,42 +1,99 @@
-# ////////////////////////////////////////////////////////////////////////////
-# FILE: alissa.py
-# AUTHOR: David Ruvolo
-# CREATED: 2022-04-13
-# MODIFIED: 2023-03-16
-# PURPOSE: custom Alissa public api client
-# STATUS: stable
-# PACKAGES: requests
-# COMMENTS: mini client for the Alissa Interpret Public API (v5.3) to get
-# molecular variant information per patient. The `Alissa` class is designed to
-# run in several steps that are controlled by the user. In order to extract
-# the data, you must execute these methods in the following way.
-#
-#   1. `login`: Login
-#   2. `getPatients`: Get all patients and extract patient IDs
-#   3. `getPatientAnalyses`: For each patient, get all analyses and extract
-#           analysis identifiers
-#   4. `getPatientVariantExportId`: For each analysis, get all variant exports
-#           and extract all export identifiers
-#   5. `getPatientVariantExportData`: For each export identifier, get variant
-#           data.
-#
-# Steps 3 to 5 can be run only if you have the patient, analysis, and export
-# identifiers.
-#
-# All Alissa Public API parameters listed in the documentation are supported.
-# Each method returns all records unless filters have been supplied. Additional
-# processing can be done in between each request.
-#
-# This script was adapted from the Alissa Interpret Client written by the
-# UMCU Genetics GitHub Organisation. In that version, it didn't quite work
-# for out use case as either some of the endpoints have changed since the
-# last release or they are using a different version.
-# https://github.com/UMCUGenetics/alissa_interpret_client/blob/main/alissa_interpret_client/alissa_interpret.py
-# ////////////////////////////////////////////////////////////////////////////
 
-from oauthlib.oauth2 import LegacyApplicationClient
-from requests_oauthlib import OAuth2Session
 from . utils import print2
+from os import path
+import numpy as np
+import tempfile
+import csv
+import molgenis.client as molgenis
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import LegacyApplicationClient
+
+class Molgenis(molgenis.Session):
+  def __init__(self, *args, **kwargs):
+    super(Molgenis, self).__init__(*args, **kwargs)
+    self.fileImportEndpoint = f"{self._root_url}plugin/importwizard/importFile"
+  
+  def _datatableToCsv(self, path, datatable):
+    """To CSV
+    Write datatable object as CSV file
+
+    @param path location to save the file
+    @param data datatable object
+    """
+    data = datatable.to_pandas().replace({np.nan: None})
+    data.to_csv(path, index=False, quoting=csv.QUOTE_ALL)
+    
+  def _dfToCsv(self, path, df):
+    """To CSV
+    Write pandas dataframe as CSV file
+
+    @param path location to save the file
+    @param df pandas data.frame
+    """
+    data = df.replace({np.nan: None})
+    data.to_csv(path, index=False, quoting=csv.QUOTE_ALL)
+  
+  def importDatatableAsCsv(self, pkg_entity: str, data):
+    """Import Datatable As CSV
+    Save a datatable object to as csv file and import into MOLGENIS using the
+    importFile api.
+    
+    NOTE: The File API will return a response if the file was imported. The
+    response is NOT an indicator that the file is valid. Please check the
+    entity sys_Import for more information.
+    
+    @param pkg_entity table identifier in emx format: package_entity
+    @param data a datatable object
+    
+    @return status message
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+      filepath=f"{tmpdir}/{pkg_entity}.csv"
+      self._datatableToCsv(filepath, data)
+      with open(path.abspath(filepath), 'r') as file:
+        response = self._session.post(
+          url = self.fileImportEndpoint,
+          headers = self._headers.token_header,
+          files = {'file': file},
+          params = {'action': 'add_update_existing', 'metadataAction': 'ignore'}
+        )
+        if (response.status_code // 100 ) != 2:
+          print2('Failed to import data into',pkg_entity,'(',response.status_code,')')
+        else:
+          print2('Imported data into', pkg_entity)
+        return response
+      
+  def importPandasAsCsv(self, pkg_entity, data):
+    """Import Pandas data.frame As CSV
+    Save a datatable object to as csv file and import into MOLGENIS using the
+    importFile api.
+    
+    NOTE: The File API will return a response if the file was imported. The
+    response is NOT an indicator that the file is valid. Please check the
+    entity sys_Import for more information.
+    
+    @param pkg_entity table identifier in emx format: package_entity
+    @param data a python data.frame
+    
+    @return status message
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+      filepath=f"{tmpdir}/{pkg_entity}.csv"
+      self._dfToCsv(filepath, data)
+      with open(path.abspath(filepath), 'r') as file:
+        response = self._session.post(
+          url=self.fileImportApi,
+          headers = self._headers.token_header,
+          files={'file': file},
+          params = {'action': 'add_update_existing', 'metadataAction': 'ignore'}
+        )
+        
+        if (response.status_code // 100) != 2:
+          print2('Failed to import data into',pkg_entity,'(',response.status_code,')')
+        else:
+          print2('Imported data into',pkg_entity)
+      return response
+
 
 class Alissa:
   """Alissa Interpret Public API (v5.3)"""
