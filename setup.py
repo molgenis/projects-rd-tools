0003f5d4c1b008b8a5ec5ca7d1d50c6bf2a8ac79
davidruvolo51 <davidruvolo51@gmail.com>
2023-03-17 10:52:50 +0100

    set version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ URL = 'https://github.com/molgenis/projects-rd-tools'
 EMAIL = 'd.c.ruvolo@gmail.com'
 AUTHOR = 'David Ruvolo'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.0'
+VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
